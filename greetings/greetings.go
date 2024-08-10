d316e99a8aa26a1da6a20aaaab252b42d923cbec
Seiya Yamaguchi <pierrot_service@icloud.com>
2024-08-10 18:00:58 +0900

    Changed to return an error if no name is entered.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
 
-// 指定された名前の挨拶を返す
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
+// Hello関数は指定された人物への挨拶を返す。
+func Hello(name string) (string, error) {
+    // 名前が与えられていない場合はメッセージとともにエラーを返す。
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+
+    // 名前を受け取った場合は、名前を埋め込んだ挨拶メッセージを返す。
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,23 @@ package main
 
 import (
     "fmt"
+    "log"
+
     "github.com/Yamase7/go-tutorial/greetings"
 )
 
-func main(){
-    // 挨拶のメッセージをもらって、出力をする
-    message := greetings.Hello("Gladys")
+func main() {
+    // ログエントリーの接頭辞と、時間・ソースファイル・行番号の表示を無効にするフラグなど、定義済みロガーのプロパティを設定します。
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // あいさつを要求.
+    message, err := greetings.Hello("")
+    // エラーが返ってきたらエラーログをコンソールに出力してプログラムを終了する。
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // エラーがなければ受け取ったメッセージを返す。
     fmt.Println(message)
 }
