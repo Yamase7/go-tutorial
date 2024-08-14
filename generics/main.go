f8606bc017e385a53738d31261204bca55f7ebd4
Seiya Yamaguchi <pierrot_service@icloud.com>
2024-08-15 01:39:35 +0900

    Add generics functions.

--- generics/main.go
+++ generics/main.go
@@ -18,6 +18,12 @@ func main() {
     fmt.Printf("Non-Generic Sums: %v and %v\n",
         SumInts(ints),
         SumFloats(floats))
+    fmt.Printf("Generic Sums: %v and %v\n",
+        SumIntsOrFloats[string, int64](ints),
+        SumIntsOrFloats[string, float64](floats))
+    fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+        SumIntsOrFloats(ints),
+        SumIntsOrFloats(floats))
 }
 
 // mの値を加算していく
@@ -32,6 +38,15 @@ func SumInts(m map[string]int64) int64 {
 // mの値を加算していく
 func SumFloats(m map[string]float64) float64 {
     var s float64
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
+// map の m の値をすべて合計した値を返す。IntとFloatの両方に対応している。
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+    var s V
     for _, v := range m {
         s += v
     }
